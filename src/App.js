c8d9a7a66322f378014f8af1f3b5504667b72aff
DhanushaMurugan <mdhanusha7@gmail.com>
2024-10-03 14:56:28 +0530

    router bug fixed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-
 import Homepage from "./component/Homepage";
-import "./index.css"
-import StartHerePage from './component/StartHerePage';
-import './App.css';
-import About from './component/About';
-import Recipes from './component/Recipes';
-
-
-
+import "./index.css";
+import StartHerePage from "./component/StartHerePage";
+import "./App.css";
+import About from "./component/About";
+import Recipes from "./component/Recipes";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-
           <Route path="/" element={<Homepage />} />
           <Route path="/StartHerePage" element={<StartHerePage />} />
           <Route path="/about" element={<About />} />
           <Route path="/recipes" element={<Recipes />} />
-
         </Routes>
       </Router>
     </div>
