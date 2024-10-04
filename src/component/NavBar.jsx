e75779c8f30b7aede1c084fba1522aea44b627e1
DhanushaMurugan <mdhanusha7@gmail.com>
2024-10-04 13:30:13 +0530

    to solve bug

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router";
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+
 
 function NavBar() {
   const navigate = useNavigate();
